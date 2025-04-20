3967de383cae8b7aea3c0031871697df918a14eb
Isha <67051067+ishapatell1@users.noreply.github.com>
2025-04-20 20:03:57 +0530

    feat: list the video on page

---
+++ youtube-dashboard-frontend/components/Videodetails.jsx
@@ -0,0 +1,47 @@
+import React, {  useEffect, useState } from 'react'
+
+export const Videodetails = () => {
+    const [videoData, setVideoData]= useState(null)
+    const apiKey= import.meta.env.VITE_YOUTUBE_API_KEY;
+    const videoId = import.meta.env.VITE_YOUTUBE_VIDEO_ID;
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
+    const fetchUrl = async (url) => {
+      try {
+        const res = await fetch(url);
+        const data = await  res.json();
+        console.log(data);
+        setVideoData(data.items[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    useEffect(()=>{
+      fetchUrl(url);
+    },[])
+   if(!videoData) return <h1>Loading...</h1>
+   const formatDate = (isoDateString) => {
+    const date = new Date(isoDateString);
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  };
+  return (
+    // <div>
+    //     <h2>{videoData.snippet.title}</h2>
+    //     <img src={videoData.snippet.thumbnails.medium.url} alt="thumbnail" />
+    //     <p>{videoData.snippet.description}</p>
+    // </div>
+    <div className="video-container">
+    <h2 className="video-title">{videoData.snippet.title}</h2>
+    <img
+      className="video-thumbnail"
+      src={videoData.snippet.thumbnails.medium.url}
+      alt="thumbnail"
+    />
+    <p className="video-description">{videoData.snippet.description}</p>
+    <p className="video-stats">
+      <strong>Published on :</strong>{formatDate(videoData.snippet.publishedAt)}
+    </p>
+  </div>
+  )
+}
--- youtube-dashboard-frontend/src/App.jsx
+++ youtube-dashboard-frontend/src/App.jsx
@@ -1,29 +1,14 @@
 import { useEffect } from "react";
 import "./App.css";
+import { Videodetails } from "../components/Videodetails";
 console.log("ALL ENV VARS", import.meta.env);
 function App() {
-   const apiKey= import.meta.env.VITE_YOUTUBE_API_KEY;
-  const videoId = import.meta.env.VITE_YOUTUBE_VIDEO_ID;
-
-const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
-
-  const fetchUrl = async (url) => {
-    try {
-      const res = await fetch(url);
-      const data = await  res.json();
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(()=>{
-    fetchUrl(url);
-  },[])
  
   return (
     <>
       <div>
         <h1>Hi, This is your youtube analytics!</h1>
+        <Videodetails/>
       </div>
     </>
   );
